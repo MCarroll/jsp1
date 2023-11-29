6bbd8c42b92a04b52950fe57940c9d69e7687278
Chris Oliver <excid3@gmail.com>
2023-11-29 15:02:51 -0600

    Must use immutable concat for postgres generated column

--- db/migrate/20230810152614_add_name_virtual_column_to_users.rb
+++ db/migrate/20230810152614_add_name_virtual_column_to_users.rb
@@ -3,9 +3,8 @@ class AddNameVirtualColumnToUsers < ActiveRecord::Migration[7.0]
     case connection.adapter_name
     when "SQLite"
       # Rails doesn't support virtual columns for SQLite yet
-      # add_column :users, :name, :virtual, type: :string, as: "first_name || ' ' || coalesce(last_name, '')", stored: true
     else
-      add_column :users, :name, :virtual, type: :string, as: "CONCAT_WS(' ', first_name, last_name)", stored: true
+      add_column :users, :name, :virtual, type: :string, as: "first_name || ' ' || coalesce(last_name, '')", stored: true
     end
   end
 end

666de74cc4ddeb0a7d6a08fff17b1c37e44d6d4f
Chris Oliver <excid3@gmail.com>
2023-11-29 16:12:42 -0600

    Use CONCAT_WS for MySQL generated columns because pipes require a sql_mode setting

--- db/migrate/20230810152614_add_name_virtual_column_to_users.rb
+++ db/migrate/20230810152614_add_name_virtual_column_to_users.rb
@@ -1,9 +1,11 @@
 class AddNameVirtualColumnToUsers < ActiveRecord::Migration[7.0]
+  # Rails doesn't support virtual columns for SQLite yet
+
   def change
     case connection.adapter_name
-    when "SQLite"
-      # Rails doesn't support virtual columns for SQLite yet
-    else
+    when "Trilogy", "Mysql2"
+      add_column :users, :name, :virtual, type: :string, as: "CONCAT_WS(' ', first_name, last_name)", stored: true
+    when "PostgreSQL"
       add_column :users, :name, :virtual, type: :string, as: "first_name || ' ' || coalesce(last_name, '')", stored: true
     end
   end

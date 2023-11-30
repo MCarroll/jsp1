75959d4985331a58b4574bfd04ebd07bf88d86d7
cjilbert504 <54157657+cjilbert504@users.noreply.github.com>
2023-11-30 15:55:15 -0600

    Refactor domains concern (#830)
    * use self instead of base
    
    * drop self call in validations
    
    * Revert "drop self call in validations"
    
    This reverts commit a8fc59bb8934ede7fbbaabf685e70fbf51259511.

--- app/models/concerns/account/domains.rb
+++ app/models/concerns/account/domains.rb
@@ -1,12 +1,12 @@
 module Account::Domains
   extend ActiveSupport::Concern
 
-  included do |base|
-    base.const_set :RESERVED_DOMAINS, [Jumpstart.config.domain]
-    base.const_set :RESERVED_SUBDOMAINS, %w[app help support]
+  included do
+    self::RESERVED_DOMAINS = [Jumpstart.config.domain]
+    self::RESERVED_SUBDOMAINS = %w[app help support]
 
     # To require a domain or subdomain, add the presence validation
-    validates :domain, exclusion: {in: base.const_get(:RESERVED_DOMAINS), message: :reserved}, uniqueness: {allow_blank: true}
-    validates :subdomain, exclusion: {in: base.const_get(:RESERVED_SUBDOMAINS), message: :reserved}, format: {with: /\A[a-zA-Z0-9]+[a-zA-Z0-9\-_]*[a-zA-Z0-9]+\Z/, message: :format, allow_blank: true}, uniqueness: {allow_blank: true}
+    validates :domain, exclusion: {in: self::RESERVED_DOMAINS, message: :reserved}, uniqueness: {allow_blank: true}
+    validates :subdomain, exclusion: {in: self::RESERVED_SUBDOMAINS, message: :reserved}, format: {with: /\A[a-zA-Z0-9]+[a-zA-Z0-9\-_]*[a-zA-Z0-9]+\Z/, message: :format, allow_blank: true}, uniqueness: {allow_blank: true}
   end
 end

42dea32f9721954b6214e176e195a9073cef6920
Ajay Guthikonda <aroopchandra@gmail.com>
2024-01-15 19:32:05 -0800

    Fix xpath selector in fill_in_trix_editor method (#919)

--- test/support/system/trix.rb
+++ test/support/system/trix.rb
@@ -3,7 +3,7 @@ module TrixSystemTestHelper
 
   # fill_in_trix_editor "post_body", with: "Hello world"
   def fill_in_trix_editor(id, with:)
-    find(:xpath, "//trix-editor[@input='#{id}']").click.set(with)
+    find(:xpath, "//trix-editor[@id='#{id}']").click.set(with)
   end
 
   # find_trix_editor "post_body"

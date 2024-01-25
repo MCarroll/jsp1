84e9707922f9cb66794275dc53f638e411085267
Chris Oliver <excid3@gmail.com>
2024-01-25 12:04:07 -0600

    Standardize

--- db/migrate/20240102192139_create_noticed_tables.noticed.rb
+++ db/migrate/20240102192139_create_noticed_tables.noticed.rb
@@ -37,13 +37,13 @@ class CreateNoticedTables < ActiveRecord::Migration[6.1]
       attributes[:params] = Noticed::Coder.load(notification.params)
       attributes[:params] = {} if attributes[:params].try(:has_key?, "noticed_error") # Skip invalid records
       attributes[:notifications_attributes] = [{
-        account_id: notification.account_id, 
-        type: "#{attributes[:type]}::Notification", 
-        recipient_type: notification.recipient_type, 
-        recipient_id: notification.recipient_id, 
-        seen_at: notification.read_at, 
-        read_at: notification.interacted_at, 
-        created_at: notification.created_at, 
+        account_id: notification.account_id,
+        type: "#{attributes[:type]}::Notification",
+        recipient_type: notification.recipient_type,
+        recipient_id: notification.recipient_id,
+        seen_at: notification.read_at,
+        read_at: notification.interacted_at,
+        created_at: notification.created_at,
         updated_at: notification.updated_at
       }]
       Noticed::Event.create(attributes)

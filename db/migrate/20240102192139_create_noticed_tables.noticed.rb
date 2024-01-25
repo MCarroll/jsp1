e1ec5bb7ae520e3e54a2c8ab8b8f46f268002af2
Ryan Lundie <rlundie@gmail.com>
2024-01-25 11:54:18 -0500

    Resolve issue of missing created_at for Noticed Events after migration (#947)
    * Resolve issue of missing created_at for Noticed Events after migration
    
    This adds in the updated_at and created_at dates when migrating over from Notifications to Noticed Events.
    
    * Migrate created_at and updated_at for Noticed::Event
    
    * Spacing
    
    ---------
    
    Co-authored-by: Chris Oliver <excid3@gmail.com>

--- db/migrate/20240102192139_create_noticed_tables.noticed.rb
+++ db/migrate/20240102192139_create_noticed_tables.noticed.rb
@@ -31,12 +31,21 @@ class CreateNoticedTables < ActiveRecord::Migration[6.1]
 
     # Migrate notifications to new tables
     Notification.find_each do |notification|
-      attributes = notification.attributes.slice("type", "account_id").with_indifferent_access
+      attributes = notification.attributes.slice("type", "account_id", "created_at", "updated_at").with_indifferent_access
       attributes[:type] = "Account::AcceptedInviteNotifier" if attributes[:type] == "AcceptedInvite"
       attributes[:type] = attributes[:type].sub("Notification", "Notifier")
       attributes[:params] = Noticed::Coder.load(notification.params)
       attributes[:params] = {} if attributes[:params].try(:has_key?, "noticed_error") # Skip invalid records
-      attributes[:notifications_attributes] = [{account_id: notification.account_id, type: "#{attributes[:type]}::Notification", recipient_type: notification.recipient_type, recipient_id: notification.recipient_id, seen_at: notification.read_at, read_at: notification.interacted_at}]
+      attributes[:notifications_attributes] = [{
+        account_id: notification.account_id, 
+        type: "#{attributes[:type]}::Notification", 
+        recipient_type: notification.recipient_type, 
+        recipient_id: notification.recipient_id, 
+        seen_at: notification.read_at, 
+        read_at: notification.interacted_at, 
+        created_at: notification.created_at, 
+        updated_at: notification.updated_at
+      }]
       Noticed::Event.create(attributes)
     end
 

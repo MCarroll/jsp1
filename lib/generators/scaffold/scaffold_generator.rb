df4eb70134f4bfe86ac0d8cef63d191303b0e910
Chris Oliver <excid3@gmail.com>
2024-02-07 09:54:50 -0600

    Update scaffold generator usage doc

--- lib/generators/scaffold/scaffold_generator.rb
+++ lib/generators/scaffold/scaffold_generator.rb
@@ -1,4 +1,7 @@
 class ScaffoldGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("templates", __dir__)
+
+  # Run rails:scaffold with the same arguments and options
   hook_for :scaffold, in: :rails, default: true, type: :boolean
 
   def add_to_navigation

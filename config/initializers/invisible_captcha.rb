408de9c3e80c87057f1a5dd924cbaa0aee18ed6d
Chris Oliver <excid3@gmail.com>
2024-03-19 10:36:58 -0500

    Disable invisible_captcha spinner in test env

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,5 +1,6 @@
 InvisibleCaptcha.setup do |config|
   config.timestamp_enabled = !Rails.env.test?
+  config.spinner_enabled = !Rails.env.test?
 
   if Rails.env.test?
     config.honeypots = ["honeypotx"]

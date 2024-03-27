906ebc3f9dee7412ea6849cb51c3490dabebed3b
Chris Oliver <excid3@gmail.com>
2024-03-27 16:14:58 -0500

    Catch all request formats in errors controller. #1068

--- lib/jumpstart/app/controllers/errors_controller.rb
+++ lib/jumpstart/app/controllers/errors_controller.rb
@@ -2,10 +2,16 @@ class ErrorsController < ApplicationController
   # Copy this file to your app directory to customize
 
   def not_found
-    render status: 404
+    respond_to do |format|
+      format.json { render status: :not_found }
+      format.any { render status: :not_found, formats: :html }
+    end
   end
 
   def internal_server_error
-    render status: 500
+    respond_to do |format|
+      format.json { render status: :internal_server_error }
+      format.any { render status: :internal_server_error, formats: :html }
+    end
   end
 end

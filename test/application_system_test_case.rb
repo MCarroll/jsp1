5baf01766618f9501b9db95f0edb8bf96e0a63ba
Chris Oliver <excid3@gmail.com>
2024-04-29 12:08:14 -0500

    Standardize

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -12,7 +12,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV["CAPYBARA_SERVER_PORT"]
     served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
 
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
       browser: :remote,
       url: "http://#{ENV["SELENIUM_HOST"]}:4444"
     }

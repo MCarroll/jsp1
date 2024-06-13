fd915df113312cbe4e61328d4e7480f4854e9a59
Chris Oliver <excid3@gmail.com>
2024-06-13 15:42:21 -0500

    Refactor blur_secret to pure CSS and no JS 💪

--- app/helpers/secret_helper.rb
+++ app/helpers/secret_helper.rb
@@ -1,9 +1,12 @@
 module SecretHelper
   def blur_secret(content, blur: "blur-sm")
+    id = ["secret", SecureRandom.hex(10)].join("_")
+
     tag.div class: "inline-block relative" do
-      tag.span(content, class: "font-mono whitespace-pre-wrap #{blur}") +
-        tag.div(class: "absolute inset-0 text-center") do
-          button_tag t("reveal"), class: "btn btn-secondary btn-small m-auto", onclick: "this.parentElement.previousElementSibling.classList.remove('#{blur}');this.remove()"
+      check_box_tag(nil, nil, false, id: id, class: "hidden peer") +
+        tag.div(content, class: "blur-sm peer-checked:blur-none") +
+        label_tag(id, class: "absolute inset-0 text-center peer-checked:hidden") do
+          tag.div t("reveal"), class: "btn btn-secondary btn-xs"
         end
     end
   end

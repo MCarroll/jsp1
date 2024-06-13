2c75b87dad28ae268f26d88f3beec3aaba9fa5ff
Chris Oliver <excid3@gmail.com>
2024-06-13 10:11:19 -0500

    Standardize

--- app/helpers/secret_helper.rb
+++ app/helpers/secret_helper.rb
@@ -2,9 +2,9 @@ module SecretHelper
   def blur_secret(content, blur: "blur-sm")
     tag.div class: "inline-block relative" do
       tag.span(content, class: "font-mono whitespace-pre-wrap #{blur}") +
-      tag.div(class: "absolute inset-0 text-center") do
-        button_tag t("reveal"), class: "btn btn-secondary btn-small m-auto", onclick: "this.parentElement.previousElementSibling.classList.remove('#{blur}');this.remove()"
-      end
+        tag.div(class: "absolute inset-0 text-center") do
+          button_tag t("reveal"), class: "btn btn-secondary btn-small m-auto", onclick: "this.parentElement.previousElementSibling.classList.remove('#{blur}');this.remove()"
+        end
     end
   end
 end
